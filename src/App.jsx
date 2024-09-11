0abba9763bdcd21b820cc38af07f05e9d37fe047
Satyarshi Shukla <satyarshishukla123@gmail.com>
2024-09-11 13:15:51 +0530

    framer animation

--- src/App.jsx
+++ src/App.jsx
@@ -27,7 +27,7 @@ const App = () => {
       <div className="container" ref={containerRef}>
         <div className="fullImage">
           <motion.img
-            src="src/assets/img.svg"
+            src="/assets/img.svg"
             alt="background"
             style={{ scale: imageScale, opacity: imageOpacity }}
             transition={{ duration: 0.2 }} // Smooth transition for image
